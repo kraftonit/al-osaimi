2f2fb5ffe0028d6459e74d736b8e9953fc185041
waywise <waywisetechdevelopers@gmail.com>
2025-02-03 13:10:59 +0600

    style: about us page design in progress

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import OurMission from "@/components/about/OurMission";
+import Breadcrumb from "@/components/global/Breadcrumb";
 
 const AboutUs = () => {
   return (
     <main className="min-h-screen">
+      <Breadcrumb title="About Us" imgUrl="/images/about-breadcrumb.jpg" />
       <section className="py-16 bg-white">
         <div className="container">
           <OurMission />
